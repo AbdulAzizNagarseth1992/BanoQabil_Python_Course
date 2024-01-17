51196861ce263f96611a0a5ff0cbf61b11e43f78
AbdulAzizNagarseth1992 <65156525+AbdulAzizNagarseth1992@users.noreply.github.com>
2024-01-17 22:01:40 +0500

    Add files via upload

---
+++ exam.py
@@ -0,0 +1,37 @@
+# abdulaziznagarseth@gmail.com (03332080863)
+
+#Write a Python program to do arithmetical operations addition and division.
+
+x = int(input("Enter the First Value: ", ))
+y = int(input("Enter the Second Value: ", ))
+addition = x + y
+print(addition)
+
+e = int(input("Enter the First Value: ", ))
+f = int(input("Enter the Second Value: ", ))
+g = e/f
+print("Value of Division: ", g)
+
+#Write a Python program to find the area of a triangle
+
+b = int(input("Enter the Length of the base of Triangle:", ))
+h = int(input("Enter the height of the Triangle: ",))
+area = 1/2*b*h
+print("Area of Triangle:", area)
+
+#Write a Python program to convert Celsius to Fahrenheit.
+
+c = int(input("Enter temputare in Celcius:", ))
+t= c*9/5 + 32
+print("Celcius is equal to:", t )
+
+#Write a Python program that return all datatypes that we discussed in the class
+
+i = int(input("Enter the int:", ))
+j = float(input("Enter the float:", ))
+k = str(input("Enter the str:", ))
+l = bool(input("Enter the bool:", ))
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
